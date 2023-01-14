2fc7ed56d398267285cb2ca5f82cb18fea826817
U_M1BKT <DShokin@alfabank.ru>
2023-01-14 22:08:04 +0300

    refactor LoginPageTest

--- src/main/java/app/pages/ProductsPage.java
+++ src/main/java/app/pages/ProductsPage.java
@@ -1,15 +1,28 @@
 package app.pages;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selenide.$x;
 
 public class ProductsPage {
 
+    private final SelenideElement productsTitle = $x("//span[@class='title']");
+    private final SelenideElement menuButton = $x("//div[@class='bm-burger-button']");
     private final SelenideElement shoppingCardContainer = $x("//div[@id='shopping_cart_container']");
+    //private final ElementsCollection inventoryContainer = (ElementsCollection) $x("//div[@class='inventory_list']");
+
+    public void verifyProductsTitle() {
+        productsTitle.shouldBe(Condition.visible);
+    }
 
     public void verifyShoppingCard() {
         shoppingCardContainer.shouldBe(Condition.visible);
     }
+
+    public void addInventoryToCard() {
+        //inventoryContainer.findBy(Condition.text("Sauce Labs Bike Light"));
+    }
+
 }
--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -2,28 +2,32 @@ import app.pages.LoginPage;
 import app.pages.ProductsPage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class LoginPageTest {
 
+    private LoginPage loginPage;
+
     @BeforeEach
     public void setUp(){
         open("https://www.saucedemo.com/");
+        loginPage = new LoginPage();
     }
 
+    @DisplayName("Success login to saucedemo.com")
     @Test
     public void successLogin() {
-        LoginPage loginPage = new LoginPage();
         loginPage.login("standard_user", "secret_sauce");
         ProductsPage productsPage = new ProductsPage();
-        productsPage.verifyShoppingCard();
+        productsPage.verifyProductsTitle();
     }
 
+    @DisplayName("Authorize with incorrect password to saucedemo.com")
     @Test
     public void errorLogin() {
-        LoginPage loginPage = new LoginPage();
         loginPage.login("standard_user", "standard_user");
         Assertions.assertEquals("Epic sadface: Username and password do not match any user in this service",
                 loginPage.getErrorMessage());
