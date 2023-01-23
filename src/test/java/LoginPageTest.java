b9b1d88f1d4fccd7fb2f77736fbecb142fe176fe
U_M1BKT <DShokin@alfabank.ru>
2023-01-23 17:04:52 +0300

    add parametrized test for login

--- src/test/java/LoginPageTest.java
+++ src/test/java/LoginPageTest.java
@@ -3,6 +3,11 @@ import app.pages.ProductsPage;
 import com.codeborne.selenide.Selenide;
 import core.TestBase;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static com.codeborne.selenide.Selenide.open;
 
@@ -24,17 +29,26 @@ public class LoginPageTest extends TestBase {
         productsPage.verifyProductsTitle();
     }
 
-    @DisplayName("Authorize with locked login to saucedemo.com")
-    @Test
-    public void lockedUserLogin() {
-        loginPage.login(properties.getProperty("lockedLogin"), properties.getProperty("standardPassword"));
-        Assertions.assertEquals("Epic sadface: Sorry, this user has been locked out.",
+    @DisplayName("Authorize with incorrect data to saucedemo.com")
+    @ParameterizedTest()
+    @MethodSource("provideParameters")
+    public void incorrectUserLogin(String login, String password, String errorMessage) {
+        loginPage.login(login, password);
+        Assertions.assertEquals(errorMessage,
                 loginPage.getErrorMessage());
     }
 
     @AfterEach
     public void tearDown() {
         Selenide.closeWebDriver();
+    }
+
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of("locked_out_user", "secret_sauce", "Epic sadface: Sorry, this user has been locked out."),
+                Arguments.of("standard_user", "test", "Epic sadface: Username and password do not match any user in this service"),
+                Arguments.of("test", "test", "Epic sadface: Username and password do not match any user in this service")
+        );
     }
 
 }
